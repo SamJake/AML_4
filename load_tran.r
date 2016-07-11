a0f8c68509aee4ec9bb18662bb9e40ff6c13f0dc
Sam Jacob <Sam.Jacob@synechron.com>
2016-07-11 20:34:34 +0530

    Parametrizing date | -v10

--- load_tran.r
+++ load_tran.r
@@ -1,5 +1,5 @@
-#tran <- read.csv("E:/R/AML_4/Data/transactionDetails.csv")
-tran <- read.csv.sql("E:/R/AML_4/Data/transactionDetails.csv",sql = "select * from file where date = '11-Jan-16'")
+dt <- "12-Jan-16"
+query <- paste0("select * from file where date = '",dt,"'")
+tran <- read.csv.sql("E:/R/AML_4/Data/transactionDetails.csv",sql = query)
 tran$date <- as.Date(tran$date,format="%d-%B-%y")
-#tran$timeOfTransaction <- as.Date(tran$timeOfTransaction,format="%H:%M %p")
-#tran[is.na(tran)] <- 0
+
--- package.r
+++ package.r
@@ -1,5 +1,6 @@
 #package install
 packages <- c("devtools","factoextra","cluster","NbClust","ggplot2","sqldf")
+#packages <- c("devtools","factoextra","cluster","NbClust","ggplot2","sqldf","RHive")
 if(length(setdiff(packages,rownames(installed.packages())))>0)
 {
   install.packages(setdiff(packages,rownames(installed.packages())))
