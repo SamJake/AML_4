4981e4329036ae69b4fcfca26066b81359523739
Sam Jacob <Sam.Jacob@synechron.com>
2016-07-11 20:51:17 +0530

    Filtering out Cancelled transactions | -v11

--- load_tran.r
+++ load_tran.r
@@ -1,5 +1,6 @@
-dt <- "12-Jan-16"
-query <- paste0("select * from file where date = '",dt,"'")
+dt <- "16-Apr-16"
+stat <- "Cancelled"
+query <- paste0("select * from file where date = '",dt,"' and status != '", stat,"'")
 tran <- read.csv.sql("E:/R/AML_4/Data/transactionDetails.csv",sql = query)
 tran$date <- as.Date(tran$date,format="%d-%B-%y")
 
